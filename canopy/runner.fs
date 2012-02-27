77338a4bdfb91e6bd1ae9ac27c2aafdae4bbd51a
ot-chris-holt <cholt@technologies.net>
2012-02-26 18:51:47 -0600

    added basic test definitions and runner

---
+++ canopy/runner.fs
@@ -0,0 +1,13 @@
+﻿module runner
+
+let mutable tests = []
+
+let test f = 
+    let fAsList = [f]
+    tests <- List.append tests fAsList
+
+//do this for type inference of tests collection, not sure how to type annotate function signatures
+test (fun _ -> System.Console.WriteLine(""))
+
+let run _ =
+    List.map (fun f -> (f ())) tests
