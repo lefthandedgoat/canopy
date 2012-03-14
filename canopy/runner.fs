ef27320353cf28d430442968720be48ed216e3af
lefthandedgoat <lefthandedgoat@gmail.com>
2012-03-13 20:46:49 -0500

    added ability to failfast

--- canopy/runner.fs
+++ canopy/runner.fs
@@ -2,18 +2,26 @@
 
 let mutable tests = []
 let mutable before = fun () -> ()
+let failfast = ref false
 
 let test f = 
     let fAsList = [f]
     tests <- List.append tests fAsList
 
 let run _ =
+    let failed = ref false
     tests |> List.map (fun f -> (
-                                try
-                                    (before ())
-                                    (f ())
-                                    System.Console.WriteLine("Passed");
-                                with
-                                    | ex -> System.Console.WriteLine("Error: {0}", ex.Message);
-                                    )
+                                if failed = ref false then
+                                    try
+                                        (before ())
+                                        (f ())
+                                        System.Console.WriteLine("Passed");
+                                    with
+                                        | ex -> (
+                                                    if failfast = ref true then
+                                                        failed := true
+                                                        System.Console.WriteLine("failfast was set to true and an error occured; stopping testing");
+                                                    System.Console.WriteLine("Error: {0}", ex.Message);
+                                                )
+                                        )
                                 ) 
