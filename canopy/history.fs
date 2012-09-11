809ec5869255080807c445ac7733b9e8fb3b25bc
lefthandedgoat <lefthandedgoat@gmail.com>
2012-09-10 22:01:51 -0500

    fixed path issue

--- canopy/history.fs
+++ canopy/history.fs
@@ -3,9 +3,11 @@
 open System.IO
 open System
 
+let p = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\canopy\"
 let path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\canopy\failedContexts.txt"
 
 let save (results : string list) =
+    if Directory.Exists(p) = false then Directory.CreateDirectory(p) |> ignore
     if File.Exists(path) = false then File.Create(path).Close()
     using(new StreamWriter(path)) (fun sw ->    
         sw.Write (String.Join("|", (results |> Array.ofList)))
