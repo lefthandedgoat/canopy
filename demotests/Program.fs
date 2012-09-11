07a3c5f34a3f4fcea8226bbcf2315e1196c1c657
lefthandedgoat <lefthandedgoat@gmail.com>
2012-09-10 22:28:21 -0500

    progress on demo tests

---
+++ demotests/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.net
+
+open canopy
+open runner
+
+start firefox
+
+let page = "http://lefthandedgoat.github.com/canopy/demo/amir.html"
+
+context "demo tests"
+before (fun _ -> url page
+                 on page)
+
+test(fun _ ->
+    describe "fill out a form"
+    "#firstName" << "Mickey"
+    "#lastName" << "Mouse"
+    "#dob" << "01/02/1942"
+    click "#genderFemale"
+    "#address" << "The Big Screen"
+    "#city" << "Holywood"
+    "#state" << "CA"
+    "#zip" << "91601"
+    click ".checkbox"
+)
+
+run ()
