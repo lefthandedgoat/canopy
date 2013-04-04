229e4c2ec0f0764f235ade0f2e8bf880f1bc2e8b
Chris Holt <lefthandedgoat@gmail.com>
2013-04-04 18:36:54 -0500

    initial

---
+++ documentationtests/Program.fs
@@ -0,0 +1,14 @@
+﻿open canopy
+open runner
+
+start firefox
+
+"Go to google and search for hello world" &&& fun _ ->
+    url "http://google.com"
+    "input[type='text']" << "hello world"
+
+run()
+
+System.Console.ReadKey() |> ignore
+
+quit()
