84238516cb090dcea6be7525a458fb6f46fb0f0c
Chris Holt <lefthandedgoat@gmail.com>
2013-04-21 15:39:20 -0500

    documentation tests

--- documentationtests/Program.fs
+++ documentationtests/Program.fs
@@ -1,12 +1,37 @@
 ﻿open canopy
 open runner
+open OpenQA.Selenium
+open configuration
+open reporters
+reporter <- new LiveHtmlReporter() :> IReporter
 
-start firefox
+start chrome
 
-"Go to google and search for hello world" &&& fun _ ->
-    url "http://google.com"
-    "input[type='text']" << "hello world"
+let actions = @"file:///C:/actions.html"
 
+context "test source links"
+
+let sourceLinkCorrect index = 
+    test(fun _ ->
+        url actions
+        let e = nth index ".source"
+        let datafor = e.GetAttribute("data-for")
+        let dataline = e.GetAttribute("data-line")
+        let lineSelector = "#LC" + dataline + " span"
+        if datafor <> null && dataline <> null then
+            describe ("Test for " + datafor)
+            click e
+            on "https://github.com/lefthandedgoat/canopy/blob/master/canopy/canopy.fs"
+            lineSelector *= datafor 
+            lineSelector *= "let" 
+    )
+
+url actions
+[0 .. (elements ".source").Length]
+|> List.iter sourceLinkCorrect
+|> ignore
+()
+    
 run()
 
 System.Console.ReadKey() |> ignore
