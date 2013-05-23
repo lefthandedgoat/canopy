2ba21fca1d6cdca1176af6209ce8519e2b8b5236
lefthandedgoat <lefthandedgoat@gmail.com>
2013-05-22 21:33:00 -0500

    removed sinatra pages

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,10 +5,6 @@ task :build do
   sh 'C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe canopy.sln'
 end
 
-task :server do
-  sh "start ruby testpages/app.rb"
-end
-
 task :ui do
   sh 'basictests\bin\debug\basictests.exe'
 end
