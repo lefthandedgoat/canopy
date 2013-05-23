e00b907b234e05b426cbe65523539b7a6ee24069
Sergey Tihon <sergey.tihon@gmail.com>
2013-05-23 23:11:46 +0300

    Added initial FAKE build scripts

---
+++ AssemblyInfo.fs
@@ -0,0 +1,9 @@
+﻿module AssemblyInfo
+open System.Reflection
+
+[<assembly: AssemblyProductAttribute("canopy")>]
+[<assembly: AssemblyTitleAttribute("canopy")>]
+[<assembly: AssemblyDescriptionAttribute("A simple framework in f# on top of selenium for writing UI automation tests.")>]
+[<assembly: AssemblyVersionAttribute("0.7.5")>]
+
+()
