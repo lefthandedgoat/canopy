0723aa56560a23169caf171c79d45c64091d3fd8
lefthandedgoat <lefthandedgoat@gmail.com>
2013-07-26 13:41:48 -0500

    Built 0.7.8

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("canopy")>]
 [<assembly: AssemblyTitleAttribute("canopy")>]
 [<assembly: AssemblyDescriptionAttribute("A simple framework in f# on top of selenium for writing UI automation and tests. https://github.com/lefthandedgoat/canopy/wiki/Change-Log")>]
-[<assembly: AssemblyVersionAttribute("0.7.7")>]
+[<assembly: AssemblyVersionAttribute("0.7.8")>]
 
 ()
--- Build.fsx
+++ Build.fsx
@@ -5,7 +5,7 @@ open Fake.AssemblyInfoFile
 
 // Assembly / NuGet package properties
 let projectName = "canopy"
-let version = "0.7.7"
+let version = "0.7.8"
 let projectDescription = "A simple framework in f# on top of selenium for writing UI automation and tests. https://github.com/lefthandedgoat/canopy/wiki/Change-Log"
 let authors = ["Chris Holt"]
 
