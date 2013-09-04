eff44a422c13a1f1dc1c4db327b9463b18f2af11
lefthandedgoat <lefthandedgoat@gmail.com>
2013-09-03 23:31:13 -0500

    updated to 0.8.3

--- AssemblyInfo.fs
+++ AssemblyInfo.fs
@@ -4,6 +4,6 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("canopy")>]
 [<assembly: AssemblyTitleAttribute("canopy")>]
 [<assembly: AssemblyDescriptionAttribute("A simple framework in f# on top of selenium for writing UI automation and tests. Change Log at https://github.com/lefthandedgoat/canopy/wiki/Change-Log")>]
-[<assembly: AssemblyVersionAttribute("0.8.2")>]
+[<assembly: AssemblyVersionAttribute("0.8.3")>]
 
 ()
