2a2030d017409d8972f7825c0481c256ac689d44
Sergey Tihon <sergey.tihon@gmail.com>
2014-03-30 22:32:16 +0300

    docs from template were added + .gitignore was modified

---
+++ src/sampleprojectstructure/Pages/login.fs
@@ -0,0 +1,58 @@
+﻿module login
+
+open canopy
+open runner
+open canopyExtensions
+open config
+open common
+open home
+
+//page info
+let address = root
+
+//selectors
+let username = "#UserName"
+let password = "#Password"
+let login  = "#login_button"
+
+//helpers
+let loginAs (user : users) =    
+    //function defined in a function, nice f# feature to provide a good API
+    //called in pattern matching below
+    let login u p =        
+        if currentUrl() <> address then url address
+        username << u
+        password << p
+        click login
+        on home.Landing
+
+    match user with
+    | Administrator -> login "admin" "password!23"    
+    | Client -> login "ABCCorp" "somepassword"
+    | Manager -> login "Manager" "sljdfsljfslkfj"
+    | SalesPerson -> login "salesmanoftheyear" "moneymoneymoney"
+
+let positive _ = 
+
+    context "positive login page tests"
+
+    "login and check welcome text" &&& fun _ ->
+        url address
+        loginAs Administrator
+        welcomeText == "Weclome Administator!"
+        
+    //more tests
+
+let negative _ =
+    
+    context "positive login page tests"
+
+    "type bad password and get error" &&& fun _ ->
+        url address
+        username << "Adminstrator"
+        password << "wrongpassword"
+        modal == "Wrong password!"
+
+let all _ =
+    positive()
+    negative()
---
+++ tests/documentationtests/Program.fs
@@ -0,0 +1,43 @@
+﻿open canopy
+open runner
+open OpenQA.Selenium
+open configuration
+open reporters
+reporter <- new LiveHtmlReporter() :> IReporter
+elementTimeout <- 2.0
+compareTimeout <- 2.0
+
+start chrome
+
+let actions = @"file:///C:/actions.html"
+
+context "test source links"
+
+let sourceLinkCorrect index datafor = 
+    
+    ntest ("Test for " + datafor) (fun _ ->        
+        url actions
+        let e = nth index ".source"
+        let dataline = e.GetAttribute("data-line")
+        let lineSelector = "#LC" + dataline + " span"
+        if datafor <> null && dataline <> null then
+            click e
+            on "https://github.com/lefthandedgoat/canopy/blob/master/canopy/canopy.fs"
+            lineSelector *= datafor 
+            lineSelector *= "let" 
+    )
+
+url actions
+[0 .. (elements ".source").Length - 1]
+|> List.iter (fun index -> 
+    let e = nth index ".source"
+    let datafor = e.GetAttribute("data-for")
+    sourceLinkCorrect index datafor)
+|> ignore
+()
+    
+run()
+
+System.Console.ReadKey() |> ignore
+
+quit()
