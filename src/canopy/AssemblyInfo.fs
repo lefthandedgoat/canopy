04c8eb4601620afa1aa38c26de766a1109d95649
Chris Holt <lefthandedgoat@gmail.com>
2015-07-30 19:31:31 -0500

    Updated to latest selenium, fixes #205, adds better reporting

--- src/canopy/AssemblyInfo.fs
+++ src/canopy/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("canopy")>]
 [<assembly: AssemblyProductAttribute("canopy")>]
 [<assembly: AssemblyDescriptionAttribute("F# web testing framework")>]
-[<assembly: AssemblyVersionAttribute("0.9.29")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.29")>]
+[<assembly: AssemblyVersionAttribute("0.9.30")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.30")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.29"
+    let [<Literal>] Version = "0.9.30"
