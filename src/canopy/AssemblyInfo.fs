dad67e6fbc500411709658a9c44ed766c8f757b2
Chris Holt <lefthandedgoat@gmail.com>
2016-09-16 09:32:39 -0500

    Update release notes for 0.9.61

--- src/canopy/AssemblyInfo.fs
+++ src/canopy/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("canopy")>]
 [<assembly: AssemblyProductAttribute("canopy")>]
 [<assembly: AssemblyDescriptionAttribute("F# web testing framework")>]
-[<assembly: AssemblyVersionAttribute("0.9.60")>]
-[<assembly: AssemblyFileVersionAttribute("0.9.60")>]
+[<assembly: AssemblyVersionAttribute("0.9.61")>]
+[<assembly: AssemblyFileVersionAttribute("0.9.61")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.9.60"
-    let [<Literal>] InformationalVersion = "0.9.60"
+    let [<Literal>] Version = "0.9.61"
+    let [<Literal>] InformationalVersion = "0.9.61"
