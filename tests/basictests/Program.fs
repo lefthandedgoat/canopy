245d4f7b8645f2e3fac9620a26de102ba477e9e9
Chris Holt <lefthandedgoat@gmail.com>
2017-03-11 19:06:41 -0600

    switch to tc

--- tests/basictests/Program.fs
+++ tests/basictests/Program.fs
@@ -11,7 +11,8 @@ elementTimeout <- 3.0
 compareTimeout <- 3.0
 pageTimeout <- 3.0
 runFailedContextsFirst <- false
-reporter <- new LiveHtmlReporter(Chrome, configuration.chromeDir) :> IReporter
+reporter <- new TeamCityReporter() :> IReporter
+//reporter <- new LiveHtmlReporter(Chrome, configuration.chromeDir) :> IReporter
 reporter.setEnvironment "My Machine"
 configuration.failureMessagesThatShoulBeTreatedAsSkip <- ["Skip me when I fail"]
 
