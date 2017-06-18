14e12331c5be7deae8097d7e13920b745c3ab8bc
Chris Holt <lefthandedgoat@gmail.com>
2017-06-17 23:38:42 -0500

    Issue #371 split out json validations

--- src/canopy/csharp.fs
+++ src/canopy/csharp.fs
@@ -205,10 +205,3 @@ type canopy () =
     static member onn url = onn url
 
     static member on url = on url
-
-    static member diffJson example actual = 
-        let diff = jsonValidator.diff example actual
-        let diffString = diff |> List.map (fun d -> match d with | jsonValidator.Missing s -> sprintf "Missing %s" s | jsonValidator.Extra s -> sprintf "Extra %s" s)
-        ResizeArray<string>(diffString)
-
-    static member validateJson example actual = jsonValidator.validate example actual
