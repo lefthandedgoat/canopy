f345cabb34191c018be44b1ecd3bc17a6d009b83
Chris Holt <lefthandedgoat@gmail.com>
2017-10-11 12:28:25 -0500

    Fix #387 even better

--- src/canopy/history.fs
+++ src/canopy/history.fs
@@ -7,9 +7,10 @@ let p = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @
 let path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\canopy\failedContexts.txt"
 
 let save (results : string list) =
-    if Directory.Exists(p) = false then Directory.CreateDirectory(p) |> ignore
-    use sw = new StreamWriter(path)
-    sw.Write (String.concat "|" results)
+    if configuration.runFailedContextsFirst = true then
+        if Directory.Exists(p) = false then Directory.CreateDirectory(p) |> ignore
+        use sw = new StreamWriter(path)
+        sw.Write (String.concat "|" results)
 
 let get _ =    
     if File.Exists(path) = false then
