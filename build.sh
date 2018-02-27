e1fb1a570372ee1cb77a4e8bc19d945b694f62a5
Andreas Vilinski <vilinski@yahoo.de>
2018-02-27 15:01:19 +0100

    paket update and magic mode (#402)

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-mono .paket/paket.bootstrapper.exe
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-	exit $exit_code
-fi
-
 mono .paket/paket.exe restore -v
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
