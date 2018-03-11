ded5b76d67e116ac9dbcff1a2a9dfdab1bfb1b99
Henrik <henrik@haf.se>
2018-03-11 16:22:25 +0100

    Expose missing element- functions as extensions

--- tests/ParallelUsage/Explore.fsx
+++ tests/ParallelUsage/Explore.fsx
@@ -4,23 +4,33 @@
 #I "bin/Debug"
 #r "Argu"
 #r "WebDriver"
+open OpenQA.Selenium
+open OpenQA.Selenium.Firefox
 #r "Canopy"
 #r "Expecto"
 #r "Expecto.FsCheck"
-#load "Config.fs"
 #load "Expecto.fs"
+open Expecto
 #load "Pages.fs"
 open Canopy
 open Canopy.ParallelUsage
 open Canopy.ParallelUsage.Pages
+open System
 // TO HERE!
 
 let init () =
-    let b = start firefox
-    Context.create Config.empty browser
-let x =  init ()
+    let firefoxWithOpts =
+        let o = FirefoxOptions(AcceptInsecureCertificates = Nullable true)
+        FirefoxWithOptions o
+    let b = startWithConfigPure defaultConfig firefoxWithOpts
+    Context.createT (Some b) defaultConfig ()
 
-x.url "https://staging.qvitoo.com"
+let x = init ()
+
+x.url "https://qvitoo.dev:8080"
+x.click (x.element ".button.signup")
+x.write "#name" "Test User 1"
+x.write "#email" "test@example.com"
 
 // lastly:
 quit ()
