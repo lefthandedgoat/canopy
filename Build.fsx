ec3114c8a467b13d09829d1dcfa5b8bd8596a46c
Jeremy Bellows <jeremybellows@gmail.com>
2019-07-29 09:07:40 -0600

    #461 Changed build script to use fake instead of fsi for docs generation

--- Build.fsx
+++ Build.fsx
@@ -168,9 +168,40 @@ Target "NuGet.Integration" (fun _ ->
 // --------------------------------------------------------------------------------------
 // Generate the documentation
 
+let fakePath = "packages" @@ "FAKE" @@ "tools" @@ "FAKE.exe"
+let fakeStartInfo script workingDirectory args fsiargs environmentVars =
+    (fun (info: System.Diagnostics.ProcessStartInfo) ->
+        info.FileName <- System.IO.Path.GetFullPath fakePath
+        info.Arguments <- sprintf "%s --fsiargs -d:FAKE %s \"%s\"" args fsiargs script
+        info.WorkingDirectory <- workingDirectory
+        let setVar k v =
+            info.EnvironmentVariables.[k] <- v
+        for (k, v) in environmentVars do
+            setVar k v
+        setVar "MSBuild" msBuildExe
+        setVar "GIT" Git.CommandHelper.gitPath
+        setVar "FSI" fsiPath)
+
+/// Run the given buildscript with FAKE.exe
+let executeFAKEWithOutput workingDirectory script fsiargs envArgs =
+    let exitCode =
+        ExecProcessWithLambdas
+            (fakeStartInfo script workingDirectory "" fsiargs envArgs)
+            TimeSpan.MaxValue false ignore ignore
+    System.Threading.Thread.Sleep 1000
+    exitCode
+
+// Documentation
+let buildDocumentationTarget fsiargs target =
+    trace (sprintf "Building documentation (%s), this could take some time, please wait..." target)
+    let exit = executeFAKEWithOutput "docs/tools" "generate.fsx" fsiargs ["target", target]
+    if exit <> 0 then
+        failwith "generating reference documentation failed"
+    ()
+
 Target "GenerateDocs" (fun _ ->
-    executeFSIWithArgs "docs/tools" "generate.fsx" ["--define:RELEASE"] [] |> ignore
-)
+    buildDocumentationTarget "-D:RELEASE -d:REFERENCE" "Default"
+ )
 
 // --------------------------------------------------------------------------------------
 // Release Scripts
@@ -199,6 +230,10 @@ Target "All" DoNothing
   ==> "Build"
   ==> "RunTests"
   ==> "All"
+
+"CleanDocs"
+  ==> "GenerateDocs"
+  ==> "ReleaseDocs"
 
 "All" 
   //==> "CleanDocs"
